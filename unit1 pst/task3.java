e4c2703dbb096aeb144f99c978d93c612bd41d02
saidinesh <saidineshnalagarla@gmail.com>
2026-09-10 09:49:24 +0530

    Add files via upload

---
+++ unit1 pst/task3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int current = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            current = Math.max(arr[i], current + arr[i]);
+            max = Math.max(max, current);
+        }
+
+        System.out.println(max);
+    }
+}
+/*
+Sample Input
+8
+-2 -3 4 -1 -2 1 5 -3
+Sample Output
+7
+
+*/
---
+++ unit1 pst/task8.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        int source = sc.nextInt();
+        int destination = sc.nextInt();
+
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(source);
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            graph.get(current)
+                    .stream()
+                    .filter(node -> !visited.contains(node))
+                    .forEach(node -> {
+                        visited.add(node);
+                        queue.add(node);
+                    });
+        }
+
+        System.out.println(
+                visited.contains(destination) ? "YES" : "NO"
+        );
+    }
+}
+
+/*
+Sample Input
+5 4
+1 2
+2 3
+3 4
+4 5
+1 5
+Sample Output
+YES
+
+*/
+
